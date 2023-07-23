09abb933bdb2ed459c0d9309fbfe7b36c2a9de7f
Lens Mutombo <lensmutombo@gmail.com>
2023-07-23 16:35:45 +0200

    Api development

---
+++ api/my_work_api/my_work_api/Controllers/MyWorkController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+
+namespace my_work_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyWorkController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public MyWorkController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        [Route("GetWorks")]
+        public JsonResult GetWorks() {
+            string sqlDatasource = _configuration.GetConnectionString("myworkconnectionstring");
+            var dt = new DataTable();
+            using (SqlConnection myConnection = new SqlConnection(sqlDatasource))
+            {
+                myConnection.Open();
+                if (myConnection.State == ConnectionState.Closed)
+                    myConnection.Open();
+                var cmd = new SqlCommand("getworks", myConnection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.ExecuteNonQuery();
+                var da = new SqlDataAdapter(cmd);
+                
+                da.Fill(dt);
+                myConnection.Close();
+
+            }
+            return new JsonResult(dt);
+        }
+        [HttpPost]
+        [Route("AddWork")]
+        public JsonResult AddWork()
+        {
+            string sqlDatasource = _configuration.GetConnectionString("myworkconnectionstring");
+            var dt = new DataTable();
+            using (SqlConnection myConnection = new SqlConnection(sqlDatasource))
+            {
+                myConnection.Open();
+                if (myConnection.State == ConnectionState.Closed)
+                    myConnection.Open();
+                var cmd = new SqlCommand("addwork", myConnection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.ExecuteNonQuery();
+                var da = new SqlDataAdapter(cmd);
+
+                da.Fill(dt);
+                myConnection.Close();
+
+            }
+            return new JsonResult(dt);
+        }
+
+    }
+}
--- database/database.sql
+++ database/database.sql
@@ -10,3 +10,19 @@ create table t_work(
     constraint pk_work primary key(id)
 )
 go
+create procedure getworks
+as
+select * from t_work order by id desc
+go
+
+insert into t_work
+    (description_work)
+values
+    ('Work 1'),
+    ('Work 2'),
+    ('Work 3'),
+    ('Work 4'),
+    ('Work 5'),
+    ('Work 6'),
+    ('Work 7')
+go
